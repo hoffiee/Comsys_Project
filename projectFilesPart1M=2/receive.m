48d818ecb16e90ea3d33f5bfa4589a102c4885af
Aztrand <oscar_aspestrand@hotmail.com>
2016-02-04 00:05:06 +0100

    Nedsampling
    Gjort en minimum-distance-receiver som identifierar rätt symbol iom att
    störningar påverkar. Nästa gång blir det slicer, men lätt, då man bara
    tilldelar rätt bitkombination till symbolen som finns!

--- projectFilesPart1M=2/receive.m
+++ projectFilesPart1M=2/receive.m
@@ -23,22 +23,39 @@ alpha=0.2;  % Roll off effect
 sps=10;     % Symbol per sample
 fs=10e6;    % sample frequency
 Ts=1/fs;    
-Ns=10;      
+Ns=10; 
+const = [-5 -5/3 5/3 5];
 
 T=Ns*Ts;
 
 g=ones(1,Ns);
 MF=g;
+%MF byts sedan ut mot
+%MF = r(end:-1:1);
 %1. filter with Rx filter (Matched filter)
 
 %[];                  % Specify Rx filter here (vector)
+
 y = filter(MF,1,r);       % Here the received signal r is passed through the matched filter to obtain y 
 
 %2. Sample filter output
 
 y_sampled = y(1:Ns:length(y));             % Compute the sampled signal y_sampled
+%Placerar varje diskret diraq-spik i en vektor, då vi vet att varje spik
+%har Ns avstånd mellan sig.
+
 
 %3. Make decision on which symbol was transmitted
+
+%MDR
+for i=1:length(y_sampled)
+    for j=1:length(const)
+       D(j)= y_sampled(i)-const(j); %Calculate the distance between chosen value and the constellations
+    end
+    [M,I]=min(D)    %Decides which constellation that matches the value. (minimum distance)
+    yk(i)=const(I);
+end
+
 boundaries = [];          % Specify decision boundaries for minimum distance detection (vector)
 a_hat = [];                 % Compute the received symbols (in vector a_hat) from  
                         % the sampled signal, based on your decision
